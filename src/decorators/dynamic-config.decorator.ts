05475f2a659ca7ac4e5deecd14a17a0ed69e5a46
Halil Kaan Karakoç <kaan.karakoc@trendyol.com>
2023-07-01 20:35:40 +0300

    Fix bug in dynamic-config decarator

--- src/decorators/dynamic-config.decorator.ts
+++ src/decorators/dynamic-config.decorator.ts
@@ -1,10 +1,10 @@
 export const DYNAMIC_CONFIG_METADATA_KEY = '__dynamic_config__';
 
-export function DynamicConfig(options?: { path: string }) {
+export function DynamicConfig(prefix?: string) {
   return function (target: any) {
     Reflect.defineMetadata(
       DYNAMIC_CONFIG_METADATA_KEY,
-      { path: options?.path },
+      { prefix: prefix },
       target.prototype,
     );
   };
